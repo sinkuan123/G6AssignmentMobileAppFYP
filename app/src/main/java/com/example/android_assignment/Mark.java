0e52c6456d6e0da424c2e8b6cab7a50227d12ce4
YuHanYuan0802 <yuhanyuan0802@e.newera.edu.my>
2023-01-12 19:16:04 +0800

    created marking section with edit text

---
+++ app/src/main/java/com/example/android_assignment/Mark.java
@@ -0,0 +1,34 @@
+package com.example.android_assignment;
+
+public class Mark {
+
+    public Mark(String mytitle, String mymark, String mycomment) {
+    }
+
+    String titleInput, markInput, commentInput;
+
+    public String getTitleInput() {
+        return titleInput;
+    }
+
+    public void setTitleInput(String titleInput) {
+        this.titleInput = titleInput;
+    }
+
+    public String getMarkInput() {
+        return markInput;
+    }
+
+    public void setMarkInput(String markInput) {
+        this.markInput = markInput;
+    }
+
+    public String getCommentInput() {
+        return commentInput;
+    }
+
+    public void setCommentInput(String commentInput) {
+        this.commentInput = commentInput;
+    }
+
+}
--- app/src/main/java/com/example/android_assignment/MarkActivity.java
+++ app/src/main/java/com/example/android_assignment/MarkActivity.java
@@ -4,21 +4,101 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MarkActivity extends AppCompatActivity {
 
-    Button navHome, navMark, navWork;
+    Button navHome, navMark, navWork, comment_btn;
+    EditText title, mark, comment;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mark);
 
+        title = findViewById(R.id.titleInput);
+        mark = findViewById(R.id.markInput);
+        comment = findViewById(R.id.commentInput);
+        comment_btn = findViewById(R.id.comment_btn);
+
         navMark = findViewById(R.id.navMark);
         navHome = findViewById(R.id.navHome);
         navWork = findViewById(R.id.navWork);
+
+        comment_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String myTitle = title.getText().toString().trim();
+                String myMark = mark.getText().toString().trim();
+                String myComment = comment.getText().toString().trim();
+
+                if (myTitle.isEmpty()) {
+                    Toast.makeText(MarkActivity.this, "Enter the title!!!", Toast.LENGTH_SHORT).show();
+                }
+
+                if (myMark.isEmpty()) {
+                    Toast.makeText(MarkActivity.this, "Enter the mark!!!", Toast.LENGTH_SHORT).show();
+                }
+
+                if (myComment.isEmpty()) {
+                    Toast.makeText(MarkActivity.this, "Enter the comment!!!", Toast.LENGTH_SHORT).show();
+                }
+
+                String dbTitle = "1 Title";
+                String dbMark = "2 Mark";
+                String dbComment = "3 Comment";
+                if (i <= 9999) {
+                    i = i + 1;
+                    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = firebaseDatabase.getReference("Mark List " + i);
+                    databaseReference.child(dbTitle).setValue(myTitle).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                title.setText("");
+                                Toast.makeText(MarkActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MarkActivity.this, "Something Wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                    databaseReference.child(dbMark).setValue(myMark).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                mark.setText("");
+                                Toast.makeText(MarkActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MarkActivity.this, "Something Wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                    databaseReference.child(dbComment).setValue(myComment).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                comment.setText("");
+                                Toast.makeText(MarkActivity.this, "Updated", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(MarkActivity.this, "Something Wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         navHome.setOnClickListener(new View.OnClickListener() {
             @Override
